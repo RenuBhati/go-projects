3086862c2bd77f7efbc76d8087e1c1ba3527359d
Renu <admin@Admins-MacBook-Pro.local>
2024-11-29 22:24:28 +0530

    added code for controller and models

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -14,3 +14,4 @@ func main() {
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
+
---
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"example.com/hello/pkg/models"
+)
+
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+newBooks := models.GetAllBooks()
+res, _ := json.Marshal(NewBook)
+}
