60f12f48f4b254da1d2bc446c24b4a0dc632b59e
Renu <admin@Admins-MacBook-Pro.local>
2024-11-30 14:10:37 +0530

    added code for controller and models

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -2,14 +2,72 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"example.com/hello/pkg/models"
+	"example.com/hello/pkg/utils"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
-	res, _ := json.Marshal(NewBook)
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars()
+	bookId := vars["booId"]
+	Id, err := strconv.ParseInt(bookId, 0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ : models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request){
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request){
+	vars := mux.vars(r) // access the request from the users
+	bookId := vars["bookId"] // access the book ID
+	ID, err := strconv.ParseInt(bookId, 0, 0) //
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)			// the book we are receiving from our DB , that needs to be converted into JSON	// here we are thinking about the user , how we are going to send response
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+func UpdateBook (w http.ResponseWriter, r *http.Request){
+	// create book 
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId : vars["bookId"]
+	// getbook by ID
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	// lets find the book, if that exist that will update else not
 }
